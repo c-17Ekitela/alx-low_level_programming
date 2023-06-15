b3573df1b193dfedc50b72c73a82bdc8f2718857
c17Ekitela <emmanuelekitela2@gmail.com>
2023-06-15 13:16:25 -0700

    prinf.cc:

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  * main - Entry point
- * Description: prints "with proper grammar ,  but the outcome isa piece of 
- * art, "
  *
- * Return: always 0 (success)
- */
- * int main(void)
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
 {
-	printf("with proper grammar , but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
