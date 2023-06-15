e786349376cfbd53d6dc98840557b35653b72554
c17Ekitela <emmanuelekitela2@gmail.com>
2023-06-15 13:34:29 -0700

    quote/unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - print quote
- * Description: prints Dor's quote
- *
- * Return: Always 1 (success)
- * /
- * int main(void)
-   {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+ * main - write to stdout
+ * Return: Always 0
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
